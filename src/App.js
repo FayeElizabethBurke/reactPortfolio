6892b4474b4f16fbfc995e7a26f5839f9902a481
Faye Burke <fayeelizabethburke@gmail.com>
2019-11-29 11:20:21 +1000

    initialise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import icon from './icon.png';
 import './App.css';
+
+
+function FadeInSection(props) {
+  const [isVisible, setVisible] = React.useState(false);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+  }, []);
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={icon} className="App-logo" alt="logo" />
       </header>
+      <div className="box" ></div>
+        <FadeInSection>
+        <div className="bio">
+            <p>I'm a web developer based in Manchester.</p>
+            <p>I write clean, straight-forward and clever applications.</p>
+            <p>Talk to me!</p>
+            <p> • fayeelizabethburke@gmail.com  • </p>
+            <p> • ( +16 )0466546679  • </p>
+        </div>
+        
+        </FadeInSection>
     </div>
   );
 }
