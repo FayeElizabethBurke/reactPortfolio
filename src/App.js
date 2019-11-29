ac574913ff779a738e762932df886b6aadd62e50
Faye Burke <fayeelizabethburke@gmail.com>
2019-11-29 13:06:55 +1000

    add logos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import icon from './icon.png';
+import gitLogo from './GitHub-Mark.png';
+import codewarsLogo from './codewars.png';
+import linkedinLogo from './linkedIn.png';
 import './App.css';
 import { ProgressBar } from 'react-bootstrap';
 
@@ -22,6 +25,7 @@ function FadeInSection(props) {
   );
 }
 
+
 function App() {
   return (
     <div className="App">
@@ -29,6 +33,8 @@ function App() {
         <img src={icon} className="App-logo" alt="logo" />
       </header>
       <div className="box" ></div>
+
+      {/* bio */}
       <FadeInSection>
         <div className="bio">
           <p>I'm a web developer based in Manchester.</p>
@@ -36,34 +42,50 @@ function App() {
           <p>Talk to me!</p>
           <p> • fayeelizabethburke@gmail.com  • </p>
           <p> • ( +61 ) 0466546679  • </p>
-        </div>
-      </FadeInSection>
-      <FadeInSection>
-        <div className="box" ></div>
-        <div className="skills">
-          <h2>Skills</h2>
-          <div className="skill">
-            <ProgressBar animated variant="success" now={60} label={`JavaScript`}/>
-          </div>
-          <div className="skill">
-            <ProgressBar animated variant="info" now={65} label={`TestCafe`}/>
-          </div>
-          <div className="skill">
-            <ProgressBar animated variant="warning" now={60} label={`NodeJS`}/>
-          </div>
-          <div className="skill">
-            <ProgressBar animated variant="danger" now={50} label={`ReactJS`}/>
-          </div>
-          <div className="skill">
-            <ProgressBar animated variant="success" now={50} label={`MongoDB`}/>
-          </div>
-          <div className="skill">
-            <ProgressBar animated variant="info" now={80} label={`Git`}/>
-          </div>
-        </div>
-      </FadeInSection>
-    </div>
-  );
-}
 
-export default App;
+          <a href="https://github.com/FayeElizabethBurke" >
+          <img src={gitLogo} alt={"#"} height={'50px'}/>
+          </a>
+          <a href="https://www.linkedin.com/in/faye-burke-360138149/">
+            <img src={linkedinLogo} alt={"#"} height={'50px'}/>
+          </a>
+          <a href="https://www.codewars.com/users/FayeElizabethBurke">
+            <img src={codewarsLogo} alt={"#"} height={'50px'}/>
+          </a>
+        </div>
+                 
+
+      </FadeInSection>
+
+          {/* progress */}
+          <FadeInSection>
+            <div className="box" ></div>
+            <div className="skills">
+              <h2>Skills</h2>
+              <div className="skill">
+                <ProgressBar animated variant="success" now={60} label={`JavaScript`} />
+              </div>
+              <div className="skill">
+                <ProgressBar animated variant="info" now={65} label={`TestCafe`} />
+              </div>
+              <div className="skill">
+                <ProgressBar animated variant="warning" now={60} label={`NodeJS`} />
+              </div>
+              <div className="skill">
+                <ProgressBar animated variant="danger" now={50} label={`ReactJS`} />
+              </div>
+              <div className="skill">
+                <ProgressBar animated variant="success" now={50} label={`MongoDB`} />
+              </div>
+              <div className="skill">
+                <ProgressBar animated variant="info" now={80} label={`Git`} />
+              </div>
+            </div>
+          </FadeInSection>
+
+
+        </div>
+        );
+      }
+      
+      export default App;
